910633c2c708e49c9c7fefbb963093c6d6092762
Juan Carlos Condori <juanc.com@gmail.com>
2020-05-29 20:24:42 -0400

    primer experimento nativescript

---
+++ app/components/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <Page>
+        <ActionBar title="Hello world"/> -->
+        <GridLayout columns="*" rows="*">
+          <Image src="https://spartadev.com/resources/images/lazyraccoon.jpeg"></Image>  
+        </GridLayout>
+    </Page>
+</template>
+
+<script >
+  export default {
+    data() {
+      return {
+        msg: 'Hola Juanca! Bienvenido al futuro'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+    ActionBar {
+        background-color: #53ba82;
+        color: #ffffff;
+    }
+
+    .message {
+        vertical-align: center;
+        text-align: center;
+        font-size: 20;
+        color: #333333;
+    }
+</style>
